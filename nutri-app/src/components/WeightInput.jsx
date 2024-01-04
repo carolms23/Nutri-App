6e4c4c872d1db98351e7a025b994a69bd093607f
carolms23 <carolinems360@gmail.com>
2024-01-03 21:19:32 -0300

    :construction: feat: Adding food component

--- nutri-app/src/components/WeightInput.jsx
+++ nutri-app/src/components/WeightInput.jsx
@@ -1,14 +1,37 @@
+import {
+  FormControl,
+  MenuItem,
+  Select,
+  TextField,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+
 export default function WeightInput() {
   return (
-    <form>
-      <label >Peso em kg</label>
-      <input id="weight" type="text"></input>
-
-      <label>Pessoa</label>
-      <select id="user">
-        <option value="peixe">Peixe</option>
-        <option value="doninha">Dona</option>
-      </select>
-    </form>
+    <FormControl sx={{ m: 1, width: 150 }} class="md:flex">
+      <TextField
+        id="outlined-number"
+        label="Peso em kg"
+        size="small"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+      <TextField
+        select
+        defaultValue="Peixe"
+        variant="outlined"
+        size="small"
+        label="Pessoa"
+        sx={{width:150}}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      >
+        <MenuItem value="Peixe">Peixe</MenuItem>
+        <MenuItem value="Dona">Dona</MenuItem>
+      </TextField>
+    </FormControl>
   );
 }
